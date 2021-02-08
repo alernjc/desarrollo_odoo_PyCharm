7092f378216df1d292620c48a26fd8efe3c73e7a
alernjc <alernjc@gmail.com>
2021-02-08 08:56:25 +0000

    Custom_crm 08/02/2021

--- modulos_propios/custom_crm/models/models.py
+++ modulos_propios/custom_crm/models/models.py
@@ -5,36 +5,36 @@ from odoo.exceptions import UserError, ValidationError
 
 
 class Visit(models.Model):
-     _name = 'custom_crm.visit'   #sigue un estandar que es nombre del módulo . nombre del modelo
-     _description = 'Visit'
+    _name = 'custom_crm.visit'  # sigue un estandar que es nombre del módulo . nombre del modelo
+    _description = 'Visit'
 
-     name = fields.Char(string='Descripción')   #etiqueta que nos va a mostrar luego en las vistas. Luego en el menú, cuando abramos a cada cliente nos pondrá la descripción junto al nombre del menú
-     #customer = fields.Char(string='Cliente', required=True)  #si no ponemos el string=, nos pondría el nombre del campo
+    name = fields.Char(
+        string='Descripción')  # etiqueta que nos va a mostrar luego en las vistas. Luego en el menú, cuando abramos a cada cliente nos pondrá la descripción junto al nombre del menú
+    # customer = fields.Char(string='Cliente', required=True)  #si no ponemos el string=, nos pondría el nombre del campo
 
-     # Vamos a modelarlo ahora con relación: Varias visitas comerciales pueden estar asociadas a un cliente
-     customer = fields.Many2one (string='Cliente', comodel_name='res.partner') # comodel_name es el modelo al que hace referencia  (los clientes se almacenan en la tabla res.partner)
-     date = fields.Datetime(string="Fecha")
-     type = fields.Selection([('P','Presencial'),('W','WhatsApp'),('T','Telefónico')], string='Tipo', required=True)  #tipo de visita
-     done = fields.Boolean(string='Realizada', readonly=True)
+    # Vamos a modelarlo ahora con relación: Varias visitas comerciales pueden estar asociadas a un cliente
+    customer = fields.Many2one(string='Cliente',
+                               comodel_name='res.partner')  # comodel_name es el modelo al que hace referencia  (los clientes se almacenan en la tabla res.partner)
+    customer_description = fields.Char(string='Descripción Cliente', readonly=True)
+    date = fields.Datetime(string="Fecha")
+    type = fields.Selection([('P', 'Presencial'), ('W', 'WhatsApp'), ('T', 'Telefónico')], string='Tipo', required=True)  # tipo de visita
+    done = fields.Boolean(string='Realizada', readonly=True)
 
+    # creamos la función que cambiará el valor de done a través del botón. Al pulsar el boton definido en la vista "toggle_state" cambiará el estado
 
-     #creamos la función que cambiará el valor de done a través del botón. Al pulsar el boton definido en la vista "toggle_state" cambiará el estado
+    def toggle_state(self):
+        self.done = not self.done
 
-     def toggle_state(self):
-          self.done = not self.done
+    def action_test(self):
+        raise ValidationError("Hola Mundo")  # Esto para la aplicación, es un brake.
 
+    # vamos a crear una accion que modifique un campo
 
-     def action_test(self):
-          raise ValidationError("Hola Mundo")  #Esto para la aplicación, es un brake.
-
-     # vamos a crear una accion que modifique un campo
-
-     def action_cambiarName(self):
-          if self.customer.name_get():
-              customer_name = self.customer.name_get()[0]
-              nombre_cliente = str(customer_name[1])
-              if self.name:
-                    self.name = 'Cliente: ' + nombre_cliente + ' - Descripción: ' + self.name
-
-
-
+    def action_cambiarName(self):
+        if self.customer.name_get():
+            customer_name = self.customer.name_get()[0]  # Extraemos el nombre del campo Many2one
+            nombre_cliente = str(customer_name[1])
+            if self.name:
+                self.customer_description = 'Cliente: ' + nombre_cliente + ' - Descripción: ' + self.name
+            # if self.name:
+                  # self.name = 'Cliente: ' + nombre_cliente + ' - Descripción: ' + self.name
